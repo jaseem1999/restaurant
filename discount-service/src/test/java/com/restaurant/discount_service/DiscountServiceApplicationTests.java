e3c3dba74870b4a4e2720940a259df11144748be
jaseem1999 <ajkkalappatt@gmail.com>
2026-05-05 22:43:45 +0530

    intial commit

---
+++ discount-service/src/test/java/com/restaurant/discount_service/DiscountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.discount_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ table-service/src/main/java/com/restaurant/table_service/TableServiceApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.table_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableServiceApplication.class, args);
+	}
+
+}
