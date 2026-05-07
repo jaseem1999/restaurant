76b5db50770190b95dd3eefb4ec71163433dbd36
jaseem1999 <ajkkalappatt@gmail.com>
2026-05-08 00:19:13 +0530

    Intial branch

--- user-service/src/main/java/com/restaurant/user_service/security/SecurityConfig.java
+++ user-service/src/main/java/com/restaurant/user_service/security/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
         return http.build();
     }
     
-    // Create bean for BCryptPasswordEncoder
+
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder(12);
